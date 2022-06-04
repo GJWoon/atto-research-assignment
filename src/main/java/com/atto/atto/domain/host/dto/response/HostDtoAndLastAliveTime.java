f3e4c3a7a4d4c15da8d2d373b51fc8a89dd1ae64
GJWoon <guemjiwoon@gmail.com>
2022-06-04 14:28:08 +0900

    FIX Host alive 전체 조회 API lastAliveDate update 방식 JdbcTemplate사용

--- src/main/java/com/atto/atto/domain/host/dto/response/HostDtoAndLastAliveTime.java
+++ src/main/java/com/atto/atto/domain/host/dto/response/HostDtoAndLastAliveTime.java
@@ -5,16 +5,27 @@ import com.atto.atto.domain.host.entity.Host;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.tomcat.jni.Local;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 public class HostDtoAndLastAliveTime extends HostDto{
 
-    private String lastAliveDate;
+    private LocalDateTime lastAliveDate;
     private boolean alive;
-    public HostDtoAndLastAliveTime(Host host,boolean alive){
+    public HostDtoAndLastAliveTime(Host host){
         super(host);
-        this.lastAliveDate = String.valueOf(host.getLastAliveDate());
-        this.alive = alive;
+        this.lastAliveDate = host.getLastAliveDate();
+        this.alive = false;
+    }
 
+    public void setAlive(boolean alive){
+        this.alive = alive;
+    }
+
+    public void setLastAliveDate(){
+        this.lastAliveDate = LocalDateTime.now();
     }
 }
--- src/main/java/com/atto/atto/domain/host/service/HostQueryService.java
+++ src/main/java/com/atto/atto/domain/host/service/HostQueryService.java
@@ -9,12 +9,19 @@ import com.atto.atto.global.error.exception.BusinessException;
 import com.atto.atto.global.error.model.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,53 +32,73 @@ public class HostQueryService {
 
     private final HostRepositorySupport hostRepositorySupport;
     private final HostRepository hostRepository;
+    private final JdbcTemplate jdbcTemplate;
 
-    @Transactional
     public List<HostDtoAndLastAliveTime> findHostAliveAll(){
-        List<Host> hostList = hostRepository.findAll();
-        hostList.forEach(this::checkAlive);
-       return hostList
-               .stream()
-               .map(host ->
-                       new HostDtoAndLastAliveTime(host,checkAlive(host)
-                       )
-               ).collect(Collectors.toList());
-    }
 
+        List<Host> hostList = hostRepository.findAll();
+
+        List<HostDtoAndLastAliveTime> dtoList = new ArrayList<>();
+
+        hostList.forEach(host ->{
+            dtoList.add(checkAlive(host));
+        });
+        batchUpdateAliveDate(dtoList.stream().filter(HostDtoAndLastAliveTime::isAlive).collect(Collectors.toList())
+        );
+        return dtoList;
+    }
 
     @Transactional
     public HostDtoAndLastAliveTime findHostAliveById(Long id){
         Host host = findById(id);
-        return new HostDtoAndLastAliveTime(host,checkAlive(host));
+        HostDtoAndLastAliveTime dto = checkAlive(host);
+        if(dto.isAlive()){
+            host.updateLastAliveDate();
+        }
+         return dto;
     }
 
     public Host findById(Long id) {
         return hostRepositorySupport.findById(id);
     }
 
+    public HostDtoAndLastAliveTime checkAlive(Host host){
 
-    public boolean checkAlive(Host host){
-
-        boolean resultPingCheck = false;
-
+        HostDtoAndLastAliveTime hostDtoAndLastAliveTime = new HostDtoAndLastAliveTime(host);
         InetAddress pingCheck = null;
         try {
             pingCheck = InetAddress.getByName(host.getIp());
         } catch (UnknownHostException e) {
-            log.info("호스트의 IP 주소를 확인할 수 없음");
+            log.error("호스트의 IP 주소 {} 를 확인할 수 없음",host.getIp());
             e.printStackTrace();
-            return false;
+            return hostDtoAndLastAliveTime;
         }
 
         try {
-            if(pingCheck.isReachable(1)){
-                resultPingCheck = true;
-                host.updateLastAliveDate();
-            }
+            if(pingCheck.isReachable(1000)){
+                hostDtoAndLastAliveTime.setAlive(true);
+                hostDtoAndLastAliveTime.setLastAliveDate();
+                }
         } catch (IOException e) {
-            log.info("Ping Test 도중 에러 발생");
+            log.error("Ping Test 도중 에러 발생 ip: {}",host.getIp());
             e.printStackTrace();
         }
-        return resultPingCheck;
+        return hostDtoAndLastAliveTime;
+    }
+
+    public void batchUpdateAliveDate(List<HostDtoAndLastAliveTime> dtoList) {
+        String sql = "UPDATE t_host SET lastAliveDate = ? WHERE id = ?";
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                HostDtoAndLastAliveTime dto = dtoList.get(i);
+                ps.setTimestamp(1, Timestamp.valueOf(dto.getLastAliveDate()));
+                ps.setLong(2, dto.getId());
+            }
+            @Override
+            public int getBatchSize() {
+                return dtoList.size();
+            }
+        });
     }
 }
